b70cdabccada9f7115d53d42ac9a0a033244ae90
Catrun404 <a.kerast@gmail.com>
2021-07-07 19:47:22 +0300

    added ddl/dml for db updated application.properties according to db scheme

---
+++ db_scripts/dml_ums.sql
@@ -0,0 +1,24 @@
+-- login 'admin' password 'admin1'
+INSERT INTO ums.user_account (created_at, first_name, last_name, password, role, status, username)
+VALUES ('2021-06-30 14:13:02.257102', 'Ad', 'Min',
+        '$2a$12$5239JBqiWhtMCMMnw3nAFelXquDVLjrSFgn7KxanfNP1y1rPV2l6i', 'ADMIN', 'ACTIVE', 'admin');
+-- login 'donatello' password 'donatello1'
+INSERT INTO ums.user_account (created_at, first_name, last_name, password, role, status, username)
+VALUES ('2021-06-29 21:47:39.192087', 'Donato', 'Bardi',
+        '$2a$12$M75b1Xx6QVv7ljQxuymMf.yHXLg74naIT1FAecVM92KD5Qc9lrh5C', 'USER', 'ACTIVE', 'donatello');
+-- login 'angelo' password 'angelo1'
+INSERT INTO ums.user_account (created_at, first_name, last_name, password, role, status, username)
+VALUES ('2021-07-01 19:42:24.851992', 'Michelangelo', 'Buonarroti',
+        '$2a$12$o0hcYjOTSu3zCElE.D.PV.esfD1FPzLgxk8QiwOpYnV3h.sXMWJBG', 'USER', 'ACTIVE', 'angelo');
+-- login 'leo' password 'leo1'
+INSERT INTO ums.user_account (created_at, first_name, last_name, password, role, status, username)
+VALUES ('2021-07-01 19:59:05.991286', 'Leonardo', 'daVinci',
+        '$2a$12$36ixfrSAnGRTpWb6g6hUEOMSULswDrqnQgQke1O17TrdXXobxO80e', 'USER', 'ACTIVE', 'leo');
+-- login 'raphael' password 'raphael1'
+INSERT INTO ums.user_account (created_at, first_name, last_name, password, role, status, username)
+VALUES ('2021-07-01 20:44:58.355388', 'Raffaello', 'Santi',
+        '$2a$12$henOAykwUXZJxF.PCWaGX.cx9Z/y/u0XgL30YR8RYJT8ZdfrC5woS', 'USER', 'ACTIVE', 'raphael');
+-- login 'user' password 'user1'
+INSERT INTO ums.user_account (created_at, first_name, last_name, password, role, status, username)
+VALUES ('2021-07-07 17:52:04.518254', 'Us', 'Er', '$2a$12$yPI4lHJfb1ndonowVyVAB./xmPg/RKXY54xJarXMZtEqA6vu6C2x.',
+        'USER', 'ACTIVE', 'user');
